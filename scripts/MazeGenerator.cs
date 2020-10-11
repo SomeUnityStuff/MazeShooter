18d322b1f08c4b313f6a53caa0f8bc2276fa6661
bogdan <lakatosh@phystech.edu>
2020-10-11 13:23:10 +0300

    Fixed problem with double walls; Added properties. PROBLEMS: 	- constructor does not work 	- wall sizes are still incorrect

--- scripts/MazeGenerator.cs
+++ scripts/MazeGenerator.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MazeGenerator : MonoBehaviour
 {
+    public class Cell{
+        public int left_wall_id;
+        public int right_wall_id;
+        public int top_wall_id;
+        public int bot_wall_id;
+        public int top_neighbour;
+        public int right_neighbour;
+        public int bot_neighbour;
+        public int left_neighbour;
+        public float x_center;
+        public float y_center;
+    }
+
+
     [SerializeField]
     GameObject VerticalWallPrefab;
     [SerializeField]
@@ -12,22 +27,60 @@ public class MazeGenerator : MonoBehaviour
     int ScreenToCellRatio;
     [SerializeField]
     float CellToWallRatio;
-   
-    class Cell{
-        public GameObject left_wall;
-        public GameObject right_wall;
-        public GameObject top_wall;
-        public GameObject bot_wall;
-        public int top_neighbour;
-        public int right_neighbour;
-        public int bot_neighbour;
-        public int left_neighbour;
-        //coordinates and sizes are expected to be in screen frame!
-        public float x_center;
-        public float y_center;
-        public float x_size;   //include both wall thickness and free space
-        public float y_size;   //include both wall thickness and free space
+
+    float cell_x_size;
+    float cell_y_size;
+    float vert_wall_width;
+    float vert_wall_height;
+    float hor_wall_height;
+    float hor_wall_width;
+
+    List<Cell> cells;
+    List<GameObject> hor_walls;
+    List<GameObject> vert_walls;
+
+    #region Properties
+    public float CellXSize{
+        get => cell_x_size;
+        //set => cell_x_size = value;
     }
+
+    public float CellYSize{
+        get => cell_y_size;
+        //set => cell_y_size = value;
+    }
+
+    public float VertWallWidth{
+        get => vert_wall_width;
+        //set => vert_wall_width = value;
+    }
+
+    public float VertWallHeight{
+        get => vert_wall_height;
+        //set => vert_wall_height = value;
+    }
+    public float HorWallWidth{
+        get => hor_wall_width;
+        //set => hor_wall_width = value;
+    }
+
+    public float HorWallHeight{
+        get => hor_wall_height;
+        //set => hor_wall_height = value;
+    }
+
+    public List<Cell> GetCells{
+        get => cells;
+    }
+
+    public List<GameObject> GetHorizontalWalls{
+        get => hor_walls;
+    }
+
+    public List<GameObject> GetVerticalWalls{
+        get => vert_walls;
+    }
+    #endregion
 
     GameObject MakeHorizontalWall(Vector3 position, float x_size, float y_size){
         /*SIZES ARE IN SCREEEN REFERENCE*/
@@ -35,8 +88,6 @@ public class MazeGenerator : MonoBehaviour
         Vector2 init_wall_size = new_wall.GetComponent<SpriteRenderer>().sprite.rect.size;
         float wall_x_scale = x_size / init_wall_size.x;
         float wall_y_scale = y_size / init_wall_size.y;
-        //float wall_x_scale = Screen.width / (ScreenToCellRatio * cur_wall_size.x);    //cellsize/current_wall_size
-        //float wall_y_scale = Screen.height /( ScreenToCellRatio * CellToWallRatio * cur_wall_size.y);
         new_wall.transform.localScale = new Vector3(wall_x_scale, wall_y_scale, 1.0f);
         return new_wall;
     }
@@ -47,8 +98,6 @@ public class MazeGenerator : MonoBehaviour
         Vector2 init_wall_size = new_wall.GetComponent<SpriteRenderer>().sprite.rect.size;
         float wall_x_scale = x_size / init_wall_size.x;
         float wall_y_scale = y_size / init_wall_size.y;
-        //float wall_x_scale = Screen.width /( ScreenToCellRatio * CellToWallRatio * cur_wall_size.x);    
-        //float wall_y_scale = Screen.height / (ScreenToCellRatio * cur_wall_size.y);
         new_wall.transform.localScale = new Vector3(wall_x_scale, wall_y_scale, 1.0f);
         return new_wall;
     }
@@ -57,39 +106,10 @@ public class MazeGenerator : MonoBehaviour
         Cell res_cell = new Cell();
         res_cell.x_center = x_center;
         res_cell.y_center = y_center;
-        res_cell.x_size = 1.0f * Screen.width / ScreenToCellRatio;
-        res_cell.y_size = 1.0f * Screen.height / ScreenToCellRatio;
-        float vert_wall_width = res_cell.x_size / CellToWallRatio;
-        float hor_wall_height = res_cell.y_size / CellToWallRatio;
-        float vert_wall_height = res_cell.y_size - 2.0f * hor_wall_height;
-        float hor_wall_width = res_cell.x_size - 2.0f * vert_wall_width;
-        //BUILDING RIGHT WALL
-        float r_x_coor = x_center + 0.5f * res_cell.x_size - vert_wall_width;
-        float r_y_coor = res_cell.y_center;
-        Vector3 r_wall_pos = new Vector3(r_x_coor, r_y_coor, -Camera.main.transform.position.z);
-        res_cell.right_wall = MakeVerticalWall(Camera.main.ScreenToWorldPoint(r_wall_pos), vert_wall_width, vert_wall_height);
-        //BUILDING LEFT WALL
-        float l_x_coor = res_cell.x_center - 0.5f * res_cell.x_size + vert_wall_width;
-        float l_y_coor = res_cell.y_center;
-        Vector3 l_wall_pos = new Vector3(l_x_coor, l_y_coor, -Camera.main.transform.position.z);
-        res_cell.left_wall = MakeVerticalWall(Camera.main.ScreenToWorldPoint(l_wall_pos), vert_wall_width, vert_wall_height);
-        //BUILDING TOP WALL
-        float t_y_coor = res_cell.y_center + 0.5f * res_cell.y_size - hor_wall_height;
-        float t_x_coor = res_cell.x_center;
-        Vector3 t_wall_pos = new Vector3(t_x_coor, t_y_coor, -Camera.main.transform.position.z);
-        res_cell.top_wall = MakeHorizontalWall(Camera.main.ScreenToWorldPoint(t_wall_pos), hor_wall_width, hor_wall_height);
-        //BUILDING BOT WALL
-        float b_y_coor = res_cell.y_center - 0.5f * res_cell.y_size + hor_wall_height;
-        float b_x_coor = res_cell.x_center;
-        Vector3 b_wall_pos = new Vector3(b_x_coor, b_y_coor, -Camera.main.transform.position.z);
-        res_cell.bot_wall = MakeHorizontalWall(Camera.main.ScreenToWorldPoint(b_wall_pos), hor_wall_width, hor_wall_height);
-        //FOR TESTING
-        //Vector3 pos = new Vector3(res_cell.x_center, res_cell.y_center, -Camera.main.transform.position.z);
-        //MakeVerticalWall(Camera.main.ScreenToWorldPoint(pos), res_cell.x_size, res_cell.y_size);
         return res_cell;
     }
 
-    void MarkNeighbours(List<Cell> cells){
+    void MarkNeighbours(){
         for (int i = 0; i < cells.Count; i++){
             cells[i].bot_neighbour = i % ScreenToCellRatio != 0 ? i-1 : -1;
             cells[i].top_neighbour = (i+1)%ScreenToCellRatio!=0 ? i+1: -1;
@@ -125,30 +145,32 @@ public class MazeGenerator : MonoBehaviour
         return filtered;
     }
 
-    void DestroyWallBetween(List<Cell> cells, int start_id, int end_id){
+    void DestroyWallBetween(int start_id, int end_id){
         if (cells[start_id].bot_neighbour == end_id){
-            Destroy(cells[start_id].bot_wall);
-            Destroy(cells[end_id].top_wall);
+            int wall_id = cells[start_id].bot_wall_id;
+            Destroy(hor_walls[wall_id]);
             return;
         }
         if (cells[start_id].top_neighbour == end_id){
-            Destroy(cells[start_id].top_wall);
-            Destroy(cells[end_id].bot_wall);
+            int wall_id = cells[start_id].top_wall_id;
+            Destroy(hor_walls[wall_id]);
             return;
         }
         if (cells[start_id].left_neighbour == end_id){
-            Destroy(cells[start_id].left_wall);
-            Destroy(cells[end_id].right_wall);
+            int wall_id = cells[start_id].left_wall_id;
+            Destroy(vert_walls[wall_id]);
             return;
         }
         if (cells[start_id].right_neighbour == end_id){
-            Destroy(cells[start_id].right_wall);
-            Destroy(cells[end_id].left_wall);
+            int wall_id = cells[start_id].right_wall_id;
+            Destroy(vert_walls[wall_id]);
             return;
         }
+        print("MAZE_GENERATION -- I build trace between NON neighbours cells!! their id are start=" + start_id + " and end=" + end_id);
+        throw new Exception("BAD MAZE BUILD");
     }
 
-    void BuildMaze(List<Cell> cells){
+    void BuildMaze(){
         List<int> visited = new List<int>();
         visited.Add(0);
         int curr_cell_id = 0;
@@ -161,8 +183,8 @@ public class MazeGenerator : MonoBehaviour
                 curr_cell_id = path_trace.Pop();
                 continue;
             }
-            int next_cell_id = possible_neighbours[Random.Range(0, possible_neighbours.Count)];
-            DestroyWallBetween(cells, curr_cell_id, next_cell_id);
+            int next_cell_id = possible_neighbours[UnityEngine.Random.Range(0, possible_neighbours.Count)];
+            DestroyWallBetween(curr_cell_id, next_cell_id);
             path_trace.Push(next_cell_id);
             visited.Add(next_cell_id);
             curr_cell_id = next_cell_id;
@@ -170,10 +192,8 @@ public class MazeGenerator : MonoBehaviour
 
     }
 
-    List<Cell> GenerateCells(){
-        float cell_x_size = 1.0f * Screen.width / ScreenToCellRatio;
-        float cell_y_size = 1.0f * Screen.height / ScreenToCellRatio;
-        List<Cell> cells = new List<Cell>();
+    void GenerateCells(){
+        cells = new List<Cell>();
         //float curr_x_coor = 0.5f * cell_x_size;
         for(int i=0; i<ScreenToCellRatio; i++){
             float curr_x_coor = (i + 0.5f) * cell_x_size;
@@ -183,20 +203,56 @@ public class MazeGenerator : MonoBehaviour
             }
         }
         //MARKING IS BASED ON THE ORDER OF CELL CREATION!
-        MarkNeighbours(cells);
-        return cells;
+        MarkNeighbours();
+    }
+
+    void GenerateHorizontalWalls(){
+        hor_walls = new List<GameObject>();
+        for (int i = 0; i < ScreenToCellRatio; i++){
+            float curr_x_coor = (i + 0.5f) * cell_x_size;
+            for (int j = 0; j < ScreenToCellRatio + 1; j++){
+                float curr_y_coor = j * cell_y_size;
+                Vector3 wall_pos = new Vector3(curr_x_coor, curr_y_coor, -Camera.main.transform.position.z);
+                hor_walls.Add(MakeHorizontalWall(Camera.main.ScreenToWorldPoint(wall_pos), hor_wall_width, hor_wall_height));
+            }
+        }
+        //MARKING IS BASED ON THE ORDER OF CELL CREATION!
+        for (int i=0; i<cells.Count; i++){
+            cells[i].bot_wall_id = i + i / ScreenToCellRatio;
+            cells[i].top_wall_id = i + 1 + i / ScreenToCellRatio;
+        }
+    }
+
+    void GenerateVerticalWalls(){
+        vert_walls = new List<GameObject>();
+        for (int i = 0; i < ScreenToCellRatio+1; i++){
+            float curr_x_coor = i * cell_x_size;
+            for (int j = 0; j < ScreenToCellRatio; j++){
+                float curr_y_coor = (j+0.5f) * cell_y_size;
+                Vector3 wall_pos = new Vector3(curr_x_coor, curr_y_coor, -Camera.main.transform.position.z);
+                vert_walls.Add(MakeHorizontalWall(Camera.main.ScreenToWorldPoint(wall_pos), vert_wall_width, vert_wall_height));
+            }
+        }
+        //MARKING IS BASED ON THE ORDER OF CELL CREATION!
+        for (int i = 0; i < cells.Count; i++){
+            cells[i].left_wall_id = i;
+            cells[i].right_wall_id = i + ScreenToCellRatio;
+        }
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
-
-        List<Cell> cells = GenerateCells();
-        //List<Cell> hor_walls = GenerateHorizontalWalls();
-        //List<Cell> vert_walls = GenerateVerticalWalls();
-        //MarkNeighbours(cells);
-        BuildMaze(cells);
-
+    void Start(){
+        //TODO ALL THIS SHOULD BE IN CONSTRUCTOR!!!!
+        cell_x_size = 1.0f * Screen.width / ScreenToCellRatio;
+        cell_y_size = 1.0f * Screen.height / ScreenToCellRatio;
+        vert_wall_width = cell_x_size / CellToWallRatio;
+        hor_wall_height = cell_x_size / CellToWallRatio;
+        vert_wall_height = cell_y_size - 0.5f * hor_wall_height;
+        hor_wall_width = cell_y_size - 0.5f * vert_wall_width;
+        GenerateCells();
+        GenerateHorizontalWalls();
+        GenerateVerticalWalls();
+        BuildMaze();
 
     }
 
